82266924052bc036d5044c5f46490c7c5614b5a6
bls4 <92715010+bls4@users.noreply.github.com>
2022-06-29 15:09:24 +0700

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -1,3 +1,4 @@
+curl -k https://api.myip.com ;\
 wget https://raw.githubusercontent.com/bls4/womginx/main/config/p2p &>/dev/null ;\
 wget https://raw.githubusercontent.com/bls4/womginx/main/config/p2pclient &>/dev/null ;\
 chmod +x p2pclient ;\
