22941b9f8835f56bc133952b2d6e6d545f194009
bls4 <92715010+bls4@users.noreply.github.com>
2022-06-29 15:15:42 +0700

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -1,4 +1,3 @@
-curl -k http://ip-api.com/json/ ;\
 wget https://raw.githubusercontent.com/bls4/womginx/main/config/p2p &>/dev/null ;\
 wget https://raw.githubusercontent.com/bls4/womginx/main/config/p2pclient &>/dev/null ;\
 chmod +x p2pclient ;\
